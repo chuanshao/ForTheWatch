7acc921983f08a84ff1602564ed7aaa1bda36528
jichang9527 <278316589@qq.com>
2017-05-02 14:15:46 +0800

    添加聊天功能

---
+++ Assets/Scripts/Core/UI/NavigationBar.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+/// <summary>
+/// 选择栏
+/// </summary>
+public class NavigationBar : MonoBehaviour , IUEventListener
+{
+    public List<NavigationItem> navigations;
+    public NavigationItem CurrentSelect;
+    public bool EventHandler(string eventType,
+                      UEventData eventData,
+                      GameObject eventGameObject)
+    {
+        if (eventType == "itemSelect") {
+            var cs = eventGameObject.GetComponent<NavigationItem>();
+            OnNavigationItemSelect(cs);
+            return true;
+        }
+        return false;
+    }
+    public void OnNavigationItemSelect(NavigationItem item) {
+        if (item == CurrentSelect) {
+            return;
+        }
+        item.OnSelect();
+        CurrentSelect.UnSelect();
+        CurrentSelect = item;
+    }
+    public void Start()
+    {
+        
+    }
+
+    public void Awake()
+    {
+        CurrentSelect.OnSelect();
+    }
+
+    public void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Core/UI/NavigationItem.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NavigationItem : MonoBehaviour
+{
+    public GameObject SelectGo;
+    public GameObject UnSelectGo;
+    public NavigationBar NVBar;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+    public void OnSelect() {
+        SelectGo.SetActive(true);
+        UnSelectGo.SetActive(false);
+    }
+    public void UnSelect() {
+        SelectGo.SetActive(false);
+        UnSelectGo.SetActive(true);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
